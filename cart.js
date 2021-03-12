2ef6af91dc96ae96e48cff395b0dbae7db8af853
rahil-mittal <71699013+rahil-mittal@users.noreply.github.com>
2021-03-12 16:36:38 +0530

    Updated file with js

--- cart.js
+++ cart.js
@@ -1,16 +1,14 @@
 
 
-var objects=[{
-    id:0,
-    name:"bagpack",
-    price:39.99,
-    quantity:0
-},{
-    id:1,
-    name:"tshirt",
-    price:29.99,
-    quantity:0
-}];
+fetch('./data.json').then(response => {
+    return response.json();
+  }).then(data => {
+      buildCart(data);
+      checkout(data);
+  }).catch(err => {
+      console.log(err);
+  });
+
 
 function round(places){
 	return function(number){
@@ -18,21 +16,110 @@ function round(places){
 	};
 }
 const roundTo2=round(2);
+var totalPrice=0;
+var q=[];
+function buildCart(data){
+
+    data.forEach(({id,image,name,size,price,quantity}) =>{
+        q.push(quantity);
+        let itemContainer = document.createElement("div");
+        itemContainer.className="itemDescription";
+
+        let imgdiv = document.createElement("div");
+        imgdiv.className="descriptionValue";
+        
+        let img = document.createElement("img");
+        img.className="itemImage";
+        img.src=image;
+
+        imgdiv.appendChild(img);
 
 
-function getPrice(){
-var quan1=document.getElementById('quantity1').value;
-var quan2=document.getElementById('quantity2').value;
-var tot1=objects[0].price*quan1;
-var tot2=objects[1].price*quan2;
-objects[0].quantity=quan1;
-objects[1].quantity=quan2;
-document.getElementById("displayPrice1").innerHTML=roundTo2(tot1);
-document.getElementById("displayPrice2").innerHTML=tot2;
-document.getElementById("displayTotal").innerHTML=roundTo2(tot1+tot2);
-var myJSON1 = JSON.stringify(objects[0]);
-var myJSON2 = JSON.stringify(objects[1]);
-console.log(myJSON1);
-console.log(myJSON2);
+        let details=document.createElement("div");
+        details.className="descriptionValue";
+
+        let namee=document.createElement("span");
+        namee.innerText=name;
+
+        let sizee=document.createElement("span");
+        sizee.innerText=size;
+
+        let pricee=document.createElement("span");
+        pricee.innerText="$"+price;
+
+        details.appendChild(namee);
+        details.appendChild(sizee);
+        details.appendChild(pricee);
+
+        let quant=document.createElement("div");
+        quant.className="descriptionValue";
+        quant.classList.add("quant");
+
+        let dec=document.createElement("button");
+        dec.setAttribute("id","dec"+id);
+        
+        let t1 = document.createTextNode("-");
+
+        dec.appendChild(t1);
+        quant.appendChild(dec);
+
+        let show=document.createElement("div");
+        show.setAttribute("id","show"+id);
+        
+        show.innerText=q[id];
+        quant.appendChild(show);
+
+        
+        let inc=document.createElement("button");
+        let t2 = document.createTextNode("+");
+        inc.setAttribute("id","inc"+id);
+       
+        inc.appendChild(t2);
+        quant.appendChild(inc);
+        
+        let TPrice=document.createElement("div");
+        TPrice.className="descriptionValue";
+        TPrice.setAttribute("id","singleItem"+id);
+        TPrice.innerText='$'+roundTo2(price*q[id]);
+
+        itemContainer.appendChild(imgdiv);
+        itemContainer.appendChild(details);
+        itemContainer.appendChild(quant);
+        itemContainer.appendChild(TPrice);
+
+        document.getElementsByClassName("items")[0].appendChild(itemContainer);
+
+        document.getElementById("displayNetQuan").innerText=q[0]+q[1];
+        document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
+
+        document.getElementById("inc"+id).onclick=()=>{
+            q[id]++;
+            document.getElementById("show"+id).innerText=q[id];
+            document.getElementById("singleItem"+id).innerText='$'+roundTo2(price*q[id]);
+            data[id].quantity=q[id];
+            document.getElementById("displayNetQuan").innerText=q[0]+q[1];
+            document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
+        }
+
+        document.getElementById("dec"+id).onclick=()=>{
+            if(q[id]>0)
+            {
+                q[id]--;
+                document.getElementById("show"+id).innerText=q[id];
+                document.getElementById("singleItem"+id).innerText='$'+roundTo2(price*q[id]);
+                data[id].quantity=q[id];
+                document.getElementById("displayNetQuan").innerText=q[0]+q[1];
+                document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
+            }
+            data[id].quantity=q[id];
+        }
+            
+    });
+
+}
+
+function checkout(){
+    
+    console.log(data);
 }
 
