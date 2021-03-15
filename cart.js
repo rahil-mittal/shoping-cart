d445c96255b071aa7d719a8134a086d56d71d2ba
rahil-mittal <71699013+rahil-mittal@users.noreply.github.com>
2021-03-15 18:16:38 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -3,29 +3,74 @@
 fetch('./data.json').then(response => {
     return response.json();
   }).then(data => {
-      buildCart(data);
+      buildCart(data),
       checkout(data);
   }).catch(err => {
       console.log(err);
   });
 
 
-function round(places){
-	return function(number){
-		return Number(number.toFixed(places));
-	};
-}
-const roundTo2=round(2);
-var totalPrice=0;
+
 var q=[];
 function buildCart(data){
-
+    //var q=[];
+    
+    return function(){
     data.forEach(({id,image,name,size,price,quantity}) =>{
+        //var q=[];
         q.push(quantity);
+        //console.log(this.q);
+        //Item Container created
         let itemContainer = document.createElement("div");
         itemContainer.className="itemDescription";
 
-        let imgdiv = document.createElement("div");
+        //Img created
+        let imgdiv = createImgDiv(image);
+
+        //Deatils created
+        let details=createDetailsDiv(name,size,price);
+
+        //Quant details
+        let quant=createQuantDiv(id);
+        
+        //Total Single item price
+        let TPrice=createTPriceDiv(id,price);
+
+        itemContainer.appendChild(imgdiv);
+        itemContainer.appendChild(details);
+        itemContainer.appendChild(quant);
+        itemContainer.appendChild(TPrice);
+
+        document.getElementsByClassName("items")[0].appendChild(itemContainer);
+
+        displayNetDetails(data);
+
+        document.getElementById("inc"+id).onclick=()=>{
+            q[id]++;
+            document.getElementById("show"+id).innerText=q[id];
+            document.getElementById("singleItem"+id).innerText='$'+(price*q[id]).toFixed(2);
+            data[id].quantity=q[id];
+            displayNetDetails(data);
+        }
+
+        document.getElementById("dec"+id).onclick=()=>{
+            if(q[id]>0)
+            {
+                q[id]--;
+                document.getElementById("show"+id).innerText=q[id];
+                document.getElementById("singleItem"+id).innerText='$'+(price*q[id]).toFixed(2);
+                data[id].quantity=q[id];
+                displayNetDetails(data);
+            }
+        }
+            
+    })}();
+
+};
+
+function createImgDiv(image)
+{
+    let imgdiv=document.createElement("div");
         imgdiv.className="descriptionValue";
         
         let img = document.createElement("img");
@@ -34,8 +79,12 @@ function buildCart(data){
 
         imgdiv.appendChild(img);
 
+        return imgdiv;
+}
 
-        let details=document.createElement("div");
+function createDetailsDiv(name,size,price)
+{
+        let details =document.createElement("div");
         details.className="descriptionValue";
 
         let namee=document.createElement("span");
@@ -51,71 +100,53 @@ function buildCart(data){
         details.appendChild(sizee);
         details.appendChild(pricee);
 
-        let quant=document.createElement("div");
-        quant.className="descriptionValue";
-        quant.classList.add("quant");
+        return details;
+}
 
-        let dec=document.createElement("button");
-        dec.setAttribute("id","dec"+id);
-        
-        let t1 = document.createTextNode("-");
+function createQuantDiv(id)
+{
+    let quant=document.createElement("div");
+    quant.className="descriptionValue";
+    quant.classList.add("quant");
 
-        dec.appendChild(t1);
-        quant.appendChild(dec);
+    //Decrement button created
+    let dec=document.createElement("button");
+    dec.setAttribute("id","dec"+id);
+    let t1 = document.createTextNode("-");
+    dec.appendChild(t1);
+    quant.appendChild(dec);
 
-        let show=document.createElement("div");
-        show.setAttribute("id","show"+id);
-        
-        show.innerText=q[id];
-        quant.appendChild(show);
+    //Quntity value shown
+    let show=document.createElement("div");
+    show.setAttribute("id","show"+id);
+    show.innerText=q[id];
+    quant.appendChild(show);
 
-        
-        let inc=document.createElement("button");
-        let t2 = document.createTextNode("+");
-        inc.setAttribute("id","inc"+id);
-       
-        inc.appendChild(t2);
-        quant.appendChild(inc);
-        
-        let TPrice=document.createElement("div");
-        TPrice.className="descriptionValue";
-        TPrice.setAttribute("id","singleItem"+id);
-        TPrice.innerText='$'+roundTo2(price*q[id]);
+    //Increment button created
+    let inc=document.createElement("button");
+    let t2 = document.createTextNode("+");
+    inc.setAttribute("id","inc"+id);
+    inc.appendChild(t2);
+    quant.appendChild(inc);
 
-        itemContainer.appendChild(imgdiv);
-        itemContainer.appendChild(details);
-        itemContainer.appendChild(quant);
-        itemContainer.appendChild(TPrice);
+    return quant;
+}
 
-        document.getElementsByClassName("items")[0].appendChild(itemContainer);
+function createTPriceDiv(id,price)
+{
+    let TPrice=document.createElement("div");
+    TPrice.className="descriptionValue";
+    TPrice.setAttribute("id","singleItem"+id);
+    TPrice.innerText='$'+(price*q[id]).toFixed(2);
 
-        document.getElementById("displayNetQuan").innerText=q[0]+q[1];
-        document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
+    return TPrice;
+}
 
-        document.getElementById("inc"+id).onclick=()=>{
-            q[id]++;
-            document.getElementById("show"+id).innerText=q[id];
-            document.getElementById("singleItem"+id).innerText='$'+roundTo2(price*q[id]);
-            data[id].quantity=q[id];
-            document.getElementById("displayNetQuan").innerText=q[0]+q[1];
-            document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
-        }
-
-        document.getElementById("dec"+id).onclick=()=>{
-            if(q[id]>0)
-            {
-                q[id]--;
-                document.getElementById("show"+id).innerText=q[id];
-                document.getElementById("singleItem"+id).innerText='$'+roundTo2(price*q[id]);
-                data[id].quantity=q[id];
-                document.getElementById("displayNetQuan").innerText=q[0]+q[1];
-                document.getElementById("displayTotal").innerText='$'+roundTo2(data[0].price*q[0]+data[1].price*q[1]);
-            }
-            data[id].quantity=q[id];
-        }
-            
-    });
-
+//To display net details
+function displayNetDetails(data)
+{
+    document.getElementById("displayNetQuan").innerText=q[0]+q[1];
+    document.getElementById("displayTotal").innerText='$'+(data[0].price*q[0]+data[1].price*q[1]).toFixed(2);
 }
 
 function checkout(){
